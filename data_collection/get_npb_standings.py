957941fc28c5a2f260ed3db94f084913edc50c1f
Anna Takegawa <takegawaanna@gmail.com>
2024-03-09 15:22:38 -0500

    update: pay per win

--- data_collection/get_npb_standings.py
+++ data_collection/get_npb_standings.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import time
 import pandas as pd
+import numpy as np
 from tqdm import tqdm
 
 
@@ -27,20 +28,38 @@ def get_npb_standings_by_game(season: int, save_results=False):
 
     standings_df = pd.concat([cl_df, pl_df], ignore_index=True)
     if save_results == True:
-        standings_df.to_csv(f"standings/{season}_game_standings.csv",index=False)
+        standings_df.to_csv(
+            f"standings/{season}_game_standings.csv", index=False)
         standings_df.to_parquet(
-            f"standings/{season}_game_standings.parquet",index=False)
+            f"standings/{season}_game_standings.parquet", index=False)
 
     return standings_df
 
-    
+
+def get_final_standing(season: int, save_results=False):
+    url = f'https://spaia.jp/baseball/npb/api/official_stats?Year={season}'
+    json_data = get_json_from_url(url)
+    df = pd.DataFrame(json_data)
+    df = df.loc[(df['GameAssortment'] == '1') | (df['GameAssortment'] == '2')]
+
+    # GameAssortment = 1 -> Central league, 2 -> Pacific league
+    df['League'] = df['GameAssortment'].apply(lambda x: 'Central' if x == '1' else 'Pacific')
+    df = df.drop('GameAssortment', axis=1)
+
+    if save_results == True:
+        df.to_csv(f'standings/{season}_final_standings.csv', index=False)
+
+    return df
+
+
 if __name__ == "__main__":
-    now = datetime.now()
-    f_year = now.year - 2
-    c_year = now.year + 1
-    for i in tqdm(range(f_year,c_year)):
-        df =get_npb_standings_by_game(
-            season=i,
-            save_results=True
-        )
+    # now = datetime.now()
+    # f_year = now.year - 2
+    # c_year = now.year + 1
+    # for i in tqdm(range(f_year, c_year)):
+        # df = get_npb_standings_by_game(
+        #     season=i,
+        #     save_results=True
+        # )
+    df = get_final_standing(season=2023, save_results=True)
     print(df)
